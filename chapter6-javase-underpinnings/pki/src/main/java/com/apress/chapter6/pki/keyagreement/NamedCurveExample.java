7c48aa87cb25a8ce7dee8dad1a9d0f0b470efa3c
Thodoris Bais <thodoris.bais@gmail.com>
2021-03-03 19:48:53 +0100

    Added named curve example

---
+++ chapter6-javase-underpinnings/pki/src/main/java/com/apress/chapter6/pki/keyagreement/NamedCurveExample.java
@@ -0,0 +1,45 @@
+package com.apress.chapter6.pki.keyagreement;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.KeyAgreement;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+import java.util.Base64;
+
+/**
+ * Key agreement example using an Elliptic Curve that comes with the bundled JCE provider.
+ */
+public class NamedCurveExample {
+
+    public static void main(String[] args) throws Exception {
+
+        Security.addProvider(new BouncyCastleProvider());
+
+        KeyPairGenerator keygen = KeyPairGenerator.getInstance("ECDH", "BC");
+        ECGenParameterSpec ecSpec = new ECGenParameterSpec("sect409r1");
+        keygen.initialize(ecSpec);
+
+        KeyAgreement aKeyAgreement = KeyAgreement.getInstance("ECDH", "BC");
+        KeyPair aPair = keygen.generateKeyPair();
+        KeyAgreement bKeyAgree = KeyAgreement.getInstance("ECDH", "BC");
+        KeyPair bPair = keygen.generateKeyPair();
+
+        aKeyAgreement.init(aPair.getPrivate());
+        bKeyAgree.init(bPair.getPrivate());
+
+        aKeyAgreement.doPhase(bPair.getPublic(), true);
+        bKeyAgree.doPhase(aPair.getPublic(), true);
+
+        MessageDigest hash = MessageDigest.getInstance("SHA1", "BC");
+        byte[] aShared = hash.digest(aKeyAgreement.generateSecret());
+        byte[] bShared = hash.digest(bKeyAgree.generateSecret());
+
+        System.out.println(Base64.getEncoder().encodeToString(aShared));
+        System.out.println(Base64.getEncoder().encodeToString(bShared));
+        System.out.println("The two values "
+                + (Base64.getEncoder().encodeToString(aShared)
+                .equals(Base64.getEncoder().encodeToString(bShared))
+                ? "" : "do not ") + "match");
+    }
+}
