51f965a346e2dfa1768cf5c1c7c740beace26b0c
Thodoris Bais <thodoris.bais@gmail.com>
2021-03-03 20:32:36 +0100

    Added key generation EC example

---
+++ chapter6-javase-underpinnings/pki/src/main/java/com/apress/chapter6/pki/keygen/NamedCurveExample.java
@@ -0,0 +1,29 @@
+package com.apress.chapter6.pki.keygen;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.ECGenParameterSpec;
+
+/**
+ * Key generation example using an Elliptic Curve
+ */
+public class NamedCurveExample {
+
+    public static void main(String[] args) throws Exception {
+
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC", new BouncyCastleProvider());
+        ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v3");
+        kpg.initialize(ecSpec);
+
+        KeyPair keyPair = kpg.genKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+
+        System.out.println(publicKey.toString());
+        System.out.println(privateKey.toString());
+    }
+}
