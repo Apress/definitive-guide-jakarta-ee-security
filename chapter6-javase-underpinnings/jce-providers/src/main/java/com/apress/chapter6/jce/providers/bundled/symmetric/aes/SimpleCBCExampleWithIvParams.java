34899820d09c52558fa7694896d42ccc28747f57
Thodoris Bais <thodoris.bais@gmail.com>
2021-03-08 21:10:39 +0100

    Fixed class name

--- chapter6-javase-underpinnings/jce-providers/src/main/java/com/apress/chapter6/jce/providers/bundled/symmetric/aes/SimpleCBCExampleWithIvParams.java
+++ chapter6-javase-underpinnings/jce-providers/src/main/java/com/apress/chapter6/jce/providers/bundled/symmetric/aes/SimpleCBCExampleWithIvParams.java
@@ -7,7 +7,7 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-public class SimpleECBExampleWithIvParams {
+public class SimpleCBCExampleWithIvParams {
 
     public static void main(String[] args)
             throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException,
