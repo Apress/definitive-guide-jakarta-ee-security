d1a2d6cc73fc7a58e8c0b13e21d894f1276e4bb0
Thodoris Bais <thodoris.bais@gmail.com>
2021-03-10 19:08:54 +0100

    Added demo method to convert a BC key pair back to JCE

--- chapter6-javase-underpinnings/jce-providers/src/main/java/com/apress/chapter6/jce/providers/bouncycastle/asymmetric/RSAKeyPairGeneratorWithBC.java
+++ chapter6-javase-underpinnings/jce-providers/src/main/java/com/apress/chapter6/jce/providers/bouncycastle/asymmetric/RSAKeyPairGeneratorWithBC.java
@@ -8,13 +8,17 @@ import org.bouncycastle.crypto.util.SubjectPublicKeyInfoFactory;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 /**
- * A simple example demonstrating an RSA key pair generation utilizing Bouncy Castle.
+ * A simple example demonstrating an RSA key pair generation utilizing Bouncy Castle's light-weight API.
  */
 public class RSAKeyPairGeneratorWithBC {
 
@@ -33,10 +37,26 @@ public class RSAKeyPairGeneratorWithBC {
 
         byte[] publicKeyDer = SubjectPublicKeyInfoFactory.createSubjectPublicKeyInfo(keyPair.getPublic()).getEncoded();
         String publicKeyDerBase64 = Base64.getEncoder().encodeToString(publicKeyDer);
-        System.out.println(publicKeyDerBase64);
+        System.out.println("Public key: " + publicKeyDerBase64);
 
         byte[] privateKeyDer = PrivateKeyInfoFactory.createPrivateKeyInfo(keyPair.getPrivate()).getEncoded();
         String privateKeyDerBase64 = Base64.getEncoder().encodeToString(privateKeyDer);
-        System.out.println(privateKeyDerBase64);
+        System.out.println("Private key: " + privateKeyDerBase64);
+    }
+
+    /**
+     * This method demonstrates how to convert a BC key pair back to JCE.
+     * It is not used in this code example, but it's here to notify you on this possibility as well.
+     */
+    private static KeyPair convertBCKeyPairToJCEKeyPair(AsymmetricCipherKeyPair bcKeyPair)
+            throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+
+        byte[] pkcs8Encoded = PrivateKeyInfoFactory.createPrivateKeyInfo(bcKeyPair.getPrivate()).getEncoded();
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(pkcs8Encoded);
+        byte[] spkiEncoded = SubjectPublicKeyInfoFactory.createSubjectPublicKeyInfo(bcKeyPair.getPublic()).getEncoded();
+        X509EncodedKeySpec spkiKeySpec = new X509EncodedKeySpec(spkiEncoded);
+        KeyFactory jceKeyFactory = KeyFactory.getInstance("RSA");
+
+        return new KeyPair(jceKeyFactory.generatePublic(spkiKeySpec), jceKeyFactory.generatePrivate(pkcs8KeySpec));
     }
 }
