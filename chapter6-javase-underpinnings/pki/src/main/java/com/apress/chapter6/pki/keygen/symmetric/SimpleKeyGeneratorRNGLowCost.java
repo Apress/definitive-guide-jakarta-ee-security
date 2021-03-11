b9d20fe17551cefda79b9b9ac0293e185aed94d9
Thodoris Bais <thodoris.bais@gmail.com>
2021-03-11 14:21:48 +0100

    Added low cost key generation examples

---
+++ chapter6-javase-underpinnings/pki/src/main/java/com/apress/chapter6/pki/keygen/symmetric/SimpleKeyGeneratorRNGLowCost.java
@@ -0,0 +1,32 @@
+package com.apress.chapter6.pki.keygen.symmetric;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/**
+ * A simple key generator for AES utilizing Bouncy Castle's capabilities and strengthened by a low-cost SecureRandom generator.
+ *
+ * SecureRandom is expensive to initialize (takes several milliseconds), therefore consider keeping the instance low if you are generating many keys.
+ * This example demonstrates how to achieve this.
+ */
+public class SimpleKeyGeneratorRNGLowCost {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        SecureRandom keyRNG = new SecureRandom(); // strong random number generator (RNG)
+        byte[] keyBytes = new byte[16];
+        keyRNG.nextBytes(keyBytes);
+
+        KeyGenerator keygen = KeyGenerator.getInstance("AES");
+        keygen.init(256, keyRNG); // 128, 192, or 256 are valid AES key sizes
+
+        Key key = keygen.generateKey();
+        System.out.println("Generated key value: " + Base64.getEncoder().encodeToString(key.getEncoded()));
+    }
+
+
+}
---
+++ chapter6-javase-underpinnings/pki/src/main/java/com/apress/chapter6/pki/keygen/symmetric/SimpleSecretKeySpecGenerator.java
@@ -0,0 +1,23 @@
+package com.apress.chapter6.pki.keygen.symmetric;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/**
+ * Similar to SimpleKeyGeneratorRNGLowCost, but with a cleaner approach - demonstrates SecretKeySpec as an alternative to KeyGenerator
+ */
+public class SimpleSecretKeySpecGenerator {
+
+    public static void main(String[] args) {
+
+        SecureRandom random = new SecureRandom();
+        byte[] keyBytes = new byte[16];
+        random.nextBytes(keyBytes);
+        SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+
+        System.out.println("Generated key value: " + Base64.getEncoder().encodeToString(key.getEncoded()));
+    }
+
+
+}
