4a91fe1d7126dcd640d49e6f62af7145f782443a
Thodoris Bais <thodoris.bais@gmail.com>
2021-04-10 19:41:53 +0200

    Renamed file

--- chapter6-javase-underpinnings/jce-providers/src/main/java/com/apress/chapter6/jce/providers/ListInstalledJceProviders.java
+++ chapter6-javase-underpinnings/jce-providers/src/main/java/com/apress/chapter6/jce/providers/ListInstalledJceProviders.java
@@ -3,7 +3,7 @@ package com.apress.chapter6.jce.providers;
 import java.security.Provider;
 import java.security.Security;
 
-public class ListCurrentlyInstalledJceProviders {
+public class ListInstalledJceProviders {
     public static void main(String[] args) {
 
         for (Provider p : Security.getProviders()) {
