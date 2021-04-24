f78520736189a8a727d638211def09005e8dfa41
Thodoris Bais <thodoris.bais@gmail.com>
2021-04-24 17:36:28 +0200

    Renamed file and minor fixes

--- chapter6-javase-underpinnings/jce-providers/src/main/java/com/apress/chapter6/jce/providers/ListAvailableAlgorithmsBC.java
+++ chapter6-javase-underpinnings/jce-providers/src/main/java/com/apress/chapter6/jce/providers/ListAvailableAlgorithmsBC.java
@@ -6,20 +6,19 @@ import java.security.Provider;
 import java.security.Security;
 
 /**
- * This example lists the supported capabilities for
- * Ciphers, Key Agreements, MACs, Message Digests and Signatures,
- * and other goodies the BC provider ships with.
+ * This example lists the supported capabilities for Signatures, Ciphers, Key Agreements, MACs, Message Digests,
+ *  and other goodies the BC provider ships with.
  */
-public class ListBCSupportedCapabilities {
+public class ListAvailableAlgorithmsBC {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Security.addProvider(new BouncyCastleProvider());
         Provider provider = Security.getProvider("BC");
 
         for (Object o : provider.keySet()) {
             String entry = (String) o;
 
-            // reference to another entry
+            // alias entries refer to other entries, so let's filter them accordingly
             if (entry.startsWith("Alg.Alias."))
                 entry = entry.substring("Alg.Alias.".length());
 
